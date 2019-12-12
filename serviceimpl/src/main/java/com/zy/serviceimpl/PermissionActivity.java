e7afe044aeb92bb1b2e24f4c5bbd946e4785b0c8
zhangyang <m17722669357@163.com>
2019-12-12 15:41:47 +0800

    升级permissionsdispatcher至4.6.0

--- serviceimpl/src/main/java/com/zy/serviceimpl/PermissionActivity.java
+++ serviceimpl/src/main/java/com/zy/serviceimpl/PermissionActivity.java
@@ -1,11 +1,18 @@
 package com.zy.serviceimpl;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.PermissionRequest;
 import permissions.dispatcher.RuntimePermissions;
 
 @RuntimePermissions
@@ -15,11 +22,58 @@ public class PermissionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permission);
-        PermissionActivityPermissionsDispatcher.getContractWithPermissionCheck(this);
+        PermissionActivityPermissionsDispatcher.showCameraWithPermissionCheck(this);
     }
 
-    @NeedsPermission({Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS})
-    public void getContract() {
-        Toast.makeText(this, "获得了权限", Toast.LENGTH_SHORT).show();
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // NOTE: delegate the permission handling to generated method
+        PermissionActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+
+    @NeedsPermission({Manifest.permission.CAMERA})
+    void showCamera() {
+        Toast.makeText(this, R.string.permission_camera_allow, Toast.LENGTH_SHORT).show();
+    }
+
+//    @OnShowRationale({Manifest.permission.CAMERA})
+//    void showRationaleForCamera(PermissionRequest request) {
+////        showRationaleDialog(R.string.permission_camera_rationale, request);
+//        request.proceed();
+//    }
+
+
+    @OnPermissionDenied(Manifest.permission.CAMERA)
+    void onCameraDenied() {
+        // NOTE: Deal with a denied permission, e.g. by showing specific UI
+        // or disabling certain functionality
+        Toast.makeText(this, R.string.permission_camera_denied, Toast.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain(Manifest.permission.CAMERA)
+    void onCameraNeverAskAgain() {
+        Toast.makeText(this, R.string.permission_camera_never_askagain, Toast.LENGTH_SHORT).show();
+    }
+
+    private void showRationaleDialog(@StringRes int messageResId, final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setPositiveButton("允许", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@NonNull DialogInterface dialog, int which) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton("拒绝", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@NonNull DialogInterface dialog, int which) {
+                        request.cancel();
+                    }
+                })
+                .setCancelable(false)
+                .setMessage(messageResId)
+                .show();
     }
 }
