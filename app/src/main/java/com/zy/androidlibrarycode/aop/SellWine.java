199efb557ec23b4f33851bd2d751332f899e3949
zhangyang <m17722669357@163.com>
2020-05-26 10:17:28 +0800

    [新增]Java命令行研究

--- app/src/main/java/com/zy/androidlibrarycode/aop/SellWine.java
+++ app/src/main/java/com/zy/androidlibrarycode/aop/SellWine.java
@@ -1,13 +1,5 @@
 package com.zy.androidlibrarycode.aop;
 
-/**
- * ================================================
- * 作    者：zhangyang
- * 版    本: V4.0.1
- * 描    述：
- * ================================================
- */
-
 public interface SellWine {
 
     void mainJiu();
--- app/src/main/java/com/zy/androidlibrarycode/enums/Day.java
+++ app/src/main/java/com/zy/androidlibrarycode/enums/Day.java
@@ -1,24 +1,5 @@
 package com.zy.androidlibrarycode.enums;
 
-/**
- * ================================================
- * 作    者：zhangyang
- * 版    本: V4.0.0
- * 描    述：
- * ================================================
- * // <editor-folder desc="静态成员变量区域">
- * // </editor-folder>
- * <p>
- * // <editor-folder desc="View区域">
- * // </editor-folder>
- * <p>
- * // <editor-folder desc="对象区域">
- * // </editor-folder>
- * <p>
- * // <editor-folder desc="基础类型区域">
- * // </editor-folder>
- */
-//枚举类型，使用关键字enum
 enum Day {
     MONDAY, TUESDAY, WEDNESDAY,
     THURSDAY, FRIDAY, SATURDAY, SUNDAY
