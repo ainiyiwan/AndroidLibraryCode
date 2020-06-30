31c3f24c3f75049b46436e197bd2be51d04f6f5a
zhangyang <m17722669357@163.com>
2020-06-30 09:27:00 +0800

    [新增]1. AppLinks

---
+++ app/src/main/java/com/zy/androidlibrarycode/applink/AppLinksActivity.java
@@ -0,0 +1,16 @@
+package com.zy.androidlibrarycode.applink;
+
+import android.os.Bundle;
+
+import com.zy.androidlibrarycode.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AppLinksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_links);
+    }
+}
