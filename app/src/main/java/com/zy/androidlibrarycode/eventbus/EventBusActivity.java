24ab4441ec3e2e7534ede341bfe2869bdda669d2
zhangyang <m17722669357@163.com>
2020-11-17 15:26:59 +0800

    [新增]1. 提交EventBus，阅读源码

---
+++ app/src/main/java/com/zy/androidlibrarycode/eventbus/EventBusActivity.java
@@ -0,0 +1,36 @@
+package com.zy.androidlibrarycode.eventbus;
+
+import android.os.Bundle;
+
+import com.zy.androidlibrarycode.R;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EventBusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_bus);
+        EventBus.getDefault().post(new MessageEvent());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(MessageEvent event) {/* Do something */};
+}
