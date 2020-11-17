9de1008095c2016583c4c41c80c82f541d509afd
zhangyang <m17722669357@163.com>
2020-11-17 14:05:02 +0800

    [新增]1. 提交手写ARouter核心类 LogisticsCenter

--- app/src/main/java/com/zy/androidlibrarycode/App.java
+++ app/src/main/java/com/zy/androidlibrarycode/App.java
@@ -3,6 +3,7 @@ package com.zy.androidlibrarycode;
 import android.app.Application;
 import android.util.Log;
 
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.blankj.utilcode.util.LogUtils;
 import com.tencent.smtt.sdk.QbSdk;
 import com.zy.androidlibrarycode.closepermission.PermissionConst;
@@ -29,10 +30,10 @@ public class App extends Application {
 
         app = (App) getApplicationContext();
         if (BuildConfig.DEBUG) {           // 这两行必须写在init之前，否则这些配置在init过程中将无效
-//            ARouter.openLog();     // 打印日志
-//            ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+            ARouter.openLog();     // 打印日志
+            ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
         }
-//        ARouter.init(App.this); // 尽可能早，推荐在Application中初始化
+        ARouter.init(App.this); // 尽可能早，推荐在Application中初始化
         CustomerException.getExceptionControl().init(this);
 
         //搜集本地tbs内核信息并上报服务器，服务器返回结果决定使用哪个内核。
---
+++ app/src/main/java/com/zy/androidlibrarycode/arouter/center/Warehouse.java
@@ -0,0 +1,42 @@
+package com.zy.androidlibrarycode.arouter.center;
+
+import com.alibaba.android.arouter.base.UniqueKeyTreeMap;
+import com.alibaba.android.arouter.facade.model.RouteMeta;
+import com.alibaba.android.arouter.facade.template.IInterceptor;
+import com.alibaba.android.arouter.facade.template.IProvider;
+import com.alibaba.android.arouter.facade.template.IRouteGroup;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * ================================================
+ * 作    者：zhangyang
+ * 版    本: V4.2.0
+ * 描    述：
+ * ================================================
+ */
+class Warehouse {
+    // Cache route and metas
+    static Map<String, Class<? extends IRouteGroup>> groupsIndex = new HashMap<>();
+    static Map<String, RouteMeta> routes = new HashMap<>();
+
+    // Cache provider
+    static Map<Class, IProvider> providers = new HashMap<>();
+    static Map<String, RouteMeta> providersIndex = new HashMap<>();
+
+    // Cache interceptor
+    static Map<Integer, Class<? extends IInterceptor>> interceptorsIndex = new UniqueKeyTreeMap<>("More than one interceptors use same priority [%s]");
+    static List<IInterceptor> interceptors = new ArrayList<>();
+
+    static void clear() {
+        routes.clear();
+        groupsIndex.clear();
+        providers.clear();
+        providersIndex.clear();
+        interceptors.clear();
+        interceptorsIndex.clear();
+    }
+}
